cd188a310341eff4be8409ec48419d99c5ad5560
HiMoxY <36484725+HiMoxY@users.noreply.github.com>
2018-05-13 02:47:21 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "$";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "aimadox")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : @HiMoxY™#1027`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : @HiMoxY™#1027  ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`$|by HiMoxY™`,"https://www.twitch.tv/aimadox")
+client.user.setStatus("dnd")
+});
+
+
+
+client.login("NDQ1MDM3MTcxNjc4NzczMjYw.Ddkozg.uOvIggHdB8kj5wlD3lBdlpNhy0g");
